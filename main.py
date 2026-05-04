ad6e486a406c1c339c7a2688cc78015b01e31204
WMammoth1 <jcipriano149@gmail.com>
2026-05-04 13:38:37 -0400

    imported argparse and added user import parsing

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
     
 	load_dotenv()
+	
+	#retrieves Google Gemini API key for Gemini AI Studio from .env and sets to variable api_key
 	api_key = os.environ.get('GEMINI_API_KEY')
 
 	if api_key == None:
@@ -14,8 +17,13 @@ def main():
 
 	print("Hello from aigent!")
 
+	#creates positional argument for argparse to use with user generated input, accessed with args.user_prompt
+	parser = argparse.ArgumentParser(description="Genai Chatbot")
+	parser.add_argument("user_prompt", type=str, help="User prompt")
+	args = parser.parse_args()
+
 	response = client.models.generate_content(
-		model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+		model='gemini-2.5-flash', contents=args.user_prompt
     	)
 
 	usage_metadata = response.usage_metadata
@@ -24,6 +32,7 @@ def main():
 
 	prompt_tokens = usage_metadata.prompt_token_count
 	response_tokens = usage_metadata.candidates_token_count
+
 	
 	
 	print(f"Prompt tokens: {prompt_tokens}")
