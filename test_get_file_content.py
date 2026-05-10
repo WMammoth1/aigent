332f545195bb2851f8d108b620204410115e81c7
WMammoth1 <jcipriano149@gmail.com>
2026-05-10 12:18:29 -0400

    added calculator and get file content testing files to aigent

---
+++ test_get_file_content.py
@@ -0,0 +1,6 @@
+from functions.get_file_content import get_file_content
+
+print(f"Result for current file:\n{get_file_content('calculator', 'main.py')}")
+print(f"Result for current file:\n{get_file_content('calculator', 'pkg/calculator.py')}")
+print(f"Result for current file:\n{get_file_content('calculator', '/bin/cat')}")
+print(f"Result for current file:\n{get_file_content('calculator', 'pkg/does_not_exist.py')}")
