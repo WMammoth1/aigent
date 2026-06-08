1fa066ed936c9118c0640d8ec7444d847aeb1a9d
WMammoth1 <jcipriano149@gmail.com>
2026-06-08 10:48:34 -0400

    added agent loop functionality

--- main.py
+++ main.py
@@ -28,56 +28,64 @@ def main():
 	# creates a new list of types.content and set's user's prompt as message, note types imported from google.genai
 	messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-	response = client.models.generate_content(
-		model='gemini-2.5-flash',
-		contents=messages,
-		config=types.GenerateContentConfig(
-			tools=[available_functions],
-			system_instruction=system_prompt,
-			temperature=0),
-    	)
-	
-	usage_metadata = response.usage_metadata
+	for i in range(20):
 
-	if usage_metadata is None:
-		raise RuntimeError("failed API request")
+		response = client.models.generate_content(
+			model='gemini-2.5-flash',
+			contents=messages,
+			config=types.GenerateContentConfig(
+				tools=[available_functions],
+				system_instruction=system_prompt,
+				temperature=0),
+    		)
+	
+		for candidate in response.candidates:
+			messages.append(candidate.content)		
+
+		usage_metadata = response.usage_metadata
+
+		if usage_metadata is None:
+			raise RuntimeError("failed API request")
 
 	
-	user_prompt = args.user_prompt
-	prompt_tokens = usage_metadata.prompt_token_count
-	response_tokens = usage_metadata.candidates_token_count
+		user_prompt = args.user_prompt
+		prompt_tokens = usage_metadata.prompt_token_count
+		response_tokens = usage_metadata.candidates_token_count
 
 	
-	if args.verbose:
-		print(f"User prompt: {user_prompt}")
-		print(f"Prompt tokens: {prompt_tokens}")
-		print(f"Response tokens: {response_tokens}")
+		if args.verbose:
+			print(f"User prompt: {user_prompt}")
+			print(f"Prompt tokens: {prompt_tokens}")
+			print(f"Response tokens: {response_tokens}")
 	
 	
-	if response.function_calls:
-			
-		function_results_list = []
-		for function_call in response.function_calls:
-			function_call_result = call_function(function_call, args)
+		if response.function_calls:
+					
+			function_results_list = []
+			for function_call in response.function_calls:
+				function_call_result = call_function(function_call, args)
 
-			if not function_call_result.parts:
-				raise Exception
+				if not function_call_result.parts:
+					raise Exception
 				
 	
-			if function_call_result.parts[0].function_response == None:
-				raise Exception
+				if function_call_result.parts[0].function_response == None:
+					raise Exception
 
-			if function_call_result.parts[0].function_response.response == None:
-				raise Exception
+				if function_call_result.parts[0].function_response.response == None:
+					raise Exception
 			
-			function_results_list.append(function_call_result.parts[0])
-
-			if args.verbose == True:
-				print(f"-> {function_call_result.parts[0].function_response.response}")
+				function_results_list.append(function_call_result.parts[0])
 				
+				if args.verbose == True:
+					print(f"-> {function_call_result.parts[0].function_response.response}")
 
-	else:
-		print(response.text)
+			messages.append(types.Content(role="user", parts=function_results_list))	
+		
+
+		else:
+			print(f"{response.text}")
+			return
 	
 if __name__ == "__main__":
 	main()
